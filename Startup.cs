6ea24f36c7f26ec98b9d95fa1ad35875edc6d176
Mohammad Zmaili <42134639+mzmaili@users.noreply.github.com>
2023-01-06 09:39:11 +0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,65 @@
+using AdminLog.Entities;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ODataTest3._1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers()
+                .AddOData(opt => opt.Select().Filter().Count().OrderBy().SetMaxTop(10).AddRouteComponents("odata", GetEdmModel()))
+                .AddNewtonsoftJson();
+            services.AddODataQueryFilter();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+        private static IEdmModel GetEdmModel()
+        {
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<User>("Users");
+            return builder.GetEdmModel();
+        }
+    }
+}
---
+++ SystemDB.cs
@@ -0,0 +1,29 @@
+﻿using AdminLog.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ODataTest
+{
+    public class SystemDB : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+   => optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=AdminLogDB;User Id=postgres;Password=0000");
+
+
+        // Users Tables:
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserExtension> UserExtension { get; set; }
+        public DbSet<UserAssignedLicense> UserAssignedLicenses { get; set; }
+        public DbSet<UserAssignedPlan> UserAssignedPlans { get; set; }
+        public DbSet<UserDeviceKey> UserDeviceKeys { get; set; }
+        public DbSet<UserIdentity> UserIdentities { get; set; }
+        public DbSet<UserOnPremisesProvisioningError> UserOnPremisesProvisioningError { get; set; }
+        public DbSet<UserProvisionedPlan> UserProvisionedPlans { get; set; }
+        public DbSet<UsersCredentialUserRegistrationDetails> UsersCredentialUserRegistrationDetails { get; set; }
+        public DbSet<DirectoryRole> DirectoryRoles { get; set; }
+        public DbSet<DirectoryRoleMember> DirectoryRoleMembers { get; set; }
+    }
+}
